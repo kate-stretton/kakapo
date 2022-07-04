1356999478449aa20116f7dea8ad41b48c3d59ba
Kate Stretton <katestretton@Kates-MacBook-Air.local>
2022-07-05 10:45:07 +1200

    Add delete button to kakapo component

--- client/components/Kakapo.jsx
+++ client/components/Kakapo.jsx
@@ -1,13 +1,26 @@
 import React from 'react'
+import {removeKakapo, fetchKakapo} from '../actions'
+import {useDispatch}from 'react-redux'
 
 function Kakapo ({id, name, year}) {
+  const dispatch = useDispatch()
+
+  function handleDelete(){
+    dispatch(removeKakapo(id))
+    .then(dispatch(fetchKakapo()))
+    .catch((err) => {
+      console.log(err)
+    })
+  }
+
   return (
     <>
     <ul>
       <li>
-        {id}, {name}, {year}
+        {id}, {name}, {year} <button onClick={handleDelete}>Delete</button>
       </li>
     </ul>
+    
     </>
   )
 }
