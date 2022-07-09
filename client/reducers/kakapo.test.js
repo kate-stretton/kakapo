eea3a009f0b637fa40dd02d0921d489530ae68cf
Kate Stretton <katestretton@Kates-MacBook-Air.local>
2022-07-09 16:17:24 +1200

    Test part of kakapo reducer

---
+++ client/reducers/kakapo.test.js
@@ -0,0 +1,20 @@
+import kakapo from './kakapo'
+import { setKakapo } from '../actions/index'
+
+describe('kakapo reducer', () => {
+  it('sets kakapo data', () => {
+    const initialState = []
+    const action = setKakapo([{ id: 2, name: 'Stella', hatchYear: '2009' }])
+    const newState = kakapo(initialState, action)
+    expect.assertions(2)
+    expect(newState).toEqual(action.kakapo)
+    expect(newState[0].name).toBe('Stella')
+  })
+  it('returns default state for undefined state and no action type', () => {
+    const expectedState = []
+    const newState = kakapo(undefined, [])
+    expect.assertions(2)
+    expect(newState).toEqual(expectedState)
+    expect(newState).toHaveLength(0)
+  })
+})
