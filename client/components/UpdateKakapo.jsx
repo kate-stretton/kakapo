72455d9149ab63465942398d62415017c3502ac1
Kate Stretton <katestretton@Kates-MacBook-Air.local>
2022-07-12 14:51:33 +1200

    Changes to UpdateKakapo component

--- client/components/UpdateKakapo.jsx
+++ client/components/UpdateKakapo.jsx
@@ -2,37 +2,53 @@ import React, {useState} from 'react'
 import {useDispatch} from 'react-redux'
 import {newNameKakapo} from '../actions'
 
-function UpdateKakapo({id, year}) {
+function UpdateKakapo({id, name, hatchYear}) {
   const [showUpdateForm, setShowUpdateForm] = useState(false)
-  const [newName, setNewName] = useState('')
+  const [hideUpdate, setHideUpdate] = useState(true)
+  const initialKakapo = {
+    id: id,
+    name: name,
+    hatchYear: hatchYear
+  }
+
+  const [updateKakapo, setUpdateKakapo] = useState(initialKakapo)
+
   const dispatch = useDispatch()
 
   function showForm(e){
     e.preventDefault()
     setShowUpdateForm((prev) => !prev)
+    setHideUpdate((prev) => !prev)
   }
 
   function handleSubmit(e) {
-    console.log('component:', id, newName)
+    //console.log('component:', id, updateKakapo)
     e.preventDefault()
-    dispatch(newNameKakapo({id: id, name: newName, year: year}))
-    setNewName('')
+    dispatch(newNameKakapo(updateKakapo))
+    //setUpdateKakapo(initialKakapo)
     setShowUpdateForm((prev) => !prev)
+    setHideUpdate((prev) => !prev)
   }
 
   function handleChange(e){
-    setNewName(e.target.value)
+    setUpdateKakapo({
+      ...updateKakapo,
+      [e.target.name]: e.target.value,
+    }
+      )
   }
 
   return (
     <>
-    <button onClick={showForm}
-    >Update</button>
+    {hideUpdate && (<button onClick={showForm}
+    >Update</button>)}
       {showUpdateForm && (
         <div className='form'>
       <form onSubmit={handleSubmit}>
         <label htmlFor='updatedName'>Update name: </label>  
-        <input type='text' id ='updatedName' name='updatedName' value={newName.name} onChange={handleChange} />
+        <input type='text' id ='updatedName' name='name' value={updateKakapo.name} onChange={handleChange} />
+        <label htmlFor='updatedYear'>Update hatch year: </label>  
+        <input type='number' id ='updatedYear' name='hatchYear' value={updateKakapo.hatchYear} onChange={handleChange} />
         <button type='submit'>Update</button>
     </form></div>)}
     </>
