ae79d4eae53e7c306830049d30dbdc3f920d24b6
Kate Stretton <katestretton@Kates-MacBook-Air.local>
2022-07-12 16:07:19 +1200

    Basic CSS for search function

--- client/components/Search.jsx
+++ client/components/Search.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Search({handleSearch}) {
   return (
     <div className='search'>
-    <input onChange={(e) => handleSearch(e.target.value)}type="text" placeholder="Search for a kakapo"/>
+    <input onChange={(e) => handleSearch(e.target.value)}type="text" placeholder="Search for a kākāpō..."/>
     </div>
   )
 }
